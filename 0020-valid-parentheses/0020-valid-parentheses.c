fa17f335060f9d532c736d981ea13e1eeb13115c
Battu Yashwanth Reddy <battuyashwanthreddy@gmail.com>
2026-06-30 17:15:51 +0530

    Time: 0 ms (100%), Space: 9.5 MB (11.37%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,32 @@
+bool isValid(char* s) {
+
+    char stack[10000];
+    int top = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+
+        
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            stack[++top] = s[i];
+        }
+        
+        else {
+
+            
+            if (top == -1)
+                return false;
+
+            
+            if ((s[i] == ')' && stack[top] != '(') ||
+                (s[i] == ']' && stack[top] != '[') ||
+                (s[i] == '}' && stack[top] != '{'))
+                return false;
+
+            
+            top--;
+        }
+    }
+
+    
+    return top == -1;
+}
