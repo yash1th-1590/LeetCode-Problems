90c131685cea994dca1ce80710fa5c8311ef20fe
Battu Yashwanth Reddy <battuyashwanthreddy@gmail.com>
2026-07-06 21:40:41 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.04%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addDigits(self, num):
+        while num >= 10:
+            sum = 0
+            while num > 0:
+                sum += num % 10
+                num //= 10
+            num = sum
+        return num
