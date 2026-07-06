f49cc042a8d600a98cb8125c8a5e9cc1099b9467
Battu Yashwanth Reddy <battuyashwanthreddy@gmail.com>
2026-07-06 22:16:29 +0530

    Time: 8 ms (31.17%), Space: 12.4 MB (78.06%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        if num == 1:
+            return False
+
+        total = 1
+
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                total += i
+
+                if i != num // i:
+                    total += num // i
+
+            i += 1
+
+        return total == num
