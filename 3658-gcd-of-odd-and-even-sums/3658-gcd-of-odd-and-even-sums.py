798332b75f71abfaa1d21013e77a95ba0e78f793
Battu Yashwanth Reddy <battuyashwanthreddy@gmail.com>
2026-07-15 21:27:51 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.28%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -1,3 +1,7 @@
 class Solution(object):
     def gcdOfOddEvenSums(self, n):
-        return n 
+        sumOdd = n*n
+        sumEven = n*(n+1)
+        while sumEven!=0:
+            sumOdd,sumEven=sumEven,sumOdd%sumEven
+        return sumOdd
