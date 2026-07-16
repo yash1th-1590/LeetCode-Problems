061a67e32b930a8338ae7682603229b011030636
Battu Yashwanth Reddy <battuyashwanthreddy@gmail.com>
2026-07-16 21:20:28 +0530

    Time: 6 ms (43.55%), Space: 15.2 MB (42.45%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        five = ten = 0
+        for b in bills:
+            if b == 5:
+                five += 1
+            elif b == 10:
+                if five == 0:
+                    return False
+                five -= 1
+                ten += 1
+            else:
+                if ten and five:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
