c1ab12d9a0e4eac05a6a447065912f4619745ea1
Battu Yashwanth Reddy <battuyashwanthreddy@gmail.com>
2026-07-17 12:18:39 +0530

    Time: 1760 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,1 @@
+SELECT d.name AS Department,e.name AS Employee,e.salary AS Salary FROM Employee e JOIN Department d ON e.departmentId=d.id WHERE 3>(SELECT COUNT(DISTINCT salary) FROM Employee WHERE departmentId=e.departmentId AND salary>e.salary);
