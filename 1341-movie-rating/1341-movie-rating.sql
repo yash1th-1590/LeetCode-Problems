75beb31d97057be2d22da19d2bfee8540510c475
Battu Yashwanth Reddy <battuyashwanthreddy@gmail.com>
2026-07-29 20:16:37 +0530

    Time: 2590 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,1 @@
+(select u.name as results from users u join movierating mr on u.user_id=mr.user_id group by u.user_id,u.name order by count(*) desc,u.name limit 1) union all (select m.title as results from movies m join movierating mr on m.movie_id=mr.movie_id where mr.created_at between '2020-02-01' and '2020-02-29' group by m.movie_id,m.title order by avg(mr.rating) desc,m.title limit 1);
